13920f0e546a233ce6dcb0f5e942fa23397f2aad
Michael Kaaden <github@kaaden.net>
2022-06-24 13:28:10 +0200

    Use the generated client to load and show some books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./components/app.component";
 import { SettingsInitializerService } from "./services/settings-initializer.service";
 import { HttpClientModule } from "@angular/common/http";
+import { ApiModule, BASE_PATH } from "../../projects/book-monkey-api/src/lib";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule],
+    imports: [BrowserModule, HttpClientModule, ApiModule],
     providers: [
         {
             provide: APP_INITIALIZER,
@@ -16,6 +17,10 @@ import { HttpClientModule } from "@angular/common/http";
             },
             deps: [SettingsInitializerService],
             multi: true,
+        },
+        {
+            provide: BASE_PATH,
+            useValue: "https://api.angular.schule", // should rather take this value from the settings
         },
     ],
     bootstrap: [AppComponent],
--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { Settings } from "../models/settings";
 import { SettingsService } from "../services/settings.service";
+import { Observable } from "rxjs";
+import { Book, BooksService } from "../../../projects/book-monkey-api/src/lib";
 
 @Component({
     selector: "app-root",
@@ -10,8 +12,10 @@ import { SettingsService } from "../services/settings.service";
 export class AppComponent {
     title = "configurable-app";
     settings: Settings | undefined;
+    books$: Observable<Book[]>;
 
-    constructor(private settingsService: SettingsService) {
+    constructor(private settingsService: SettingsService, private booksService: BooksService) {
         this.settings = settingsService.settings;
+        this.books$ = booksService.booksGet();
     }
 }
